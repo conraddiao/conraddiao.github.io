f232029ea33e6e235c0526eab2f7cb30f1dd93f9
Conrad Diao <econraddiao@gmail.com>
2021-10-23 13:47:09 -0700

    removed docs init

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,19 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd docs
+#cd docs
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+#git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 git remote add origin https://github.com/econraddiao/econraddiao.github.io.git
-git branch -M main
-git push -u origin main
+#git branch -M main
+#git push -u origin main
 git push -f git@github.com:econradddiao/econraddiao.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
