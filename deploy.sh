b52e8f6ffd1fec360b5ee4a5df8018b67948b5e0
Conrad Diao <econraddiao@gmail.com>
2021-10-22 19:40:14 -0700

    ?

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git remote add origin https://github.com/econraddiao/econraddiao.github.io.git
+git branch -M main
+git push -u origin main
+git push -f git@github.com:econradddiao/econraddiao.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
