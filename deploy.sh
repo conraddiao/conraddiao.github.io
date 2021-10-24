7a349f631513b4c520de3c88c7201eafe3973f88
Conrad Diao <econraddiao@gmail.com>
2021-10-23 17:13:43 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git remote add origin https://github.com/econraddiao/econraddiao.github.io.git
+# git remote add origin https://github.com/econraddiao/econraddiao.github.io.git
 #git branch -M main
 #git push -u origin main
 #git push -f git@github.com:econradddiao/econraddiao.github.io.git main
