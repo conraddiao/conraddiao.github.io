7825f66f0c105d040cf58b3f286d998b87c7eff5
Conrad Diao <econraddiao@gmail.com>
2024-12-20 18:29:56 -0800

    trying again

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/econraddiao.github.io/', 
+  base: '/econraddiao.github.io/',
   build: {
     outDir: "docs",
+    assetsDir: "assets",
     rollupOptions: {
-      external: [
-        "vue3-carousel"
-      ]
+      external: ['vue3-carousel'],
+      output: {
+        globals: {
+          'vue3-carousel': 'vue3Carousel'
+        }
+      }
     }
   }
 })
