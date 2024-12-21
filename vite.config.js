31990babfcf5a143b7aed4d4651136a4beb1ceac
Conrad Diao <econraddiao@gmail.com>
2024-12-20 18:40:50 -0800

    mime fix again

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'copy-nojekyll',
+      writeBundle() {
+        fs.writeFileSync('docs/.nojekyll', '')
+      }
+    }
+  ],
   base: '/econraddiao.github.io/',
   build: {
     outDir: "docs",
